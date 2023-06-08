218e730c7aa421cf84997edd1588d6101c42d826
Life1sOk <mr.lifeisok@gmail.com>
2023-06-08 18:20:04 +0600

    check

--- src/blocks/login/login.block.tsx
+++ src/blocks/login/login.block.tsx
@@ -34,7 +34,7 @@ const LoginBlock = ({ changeBlock }: ILogin) => {
     };
 
     await loginUser(loginObj).catch((err) => {
-      if (err.data) setErrorMess(err.data);
+      // if (err.data) setErrorMess(err.data);
       console.log(err);
     });
   };
