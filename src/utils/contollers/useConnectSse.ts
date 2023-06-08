e1043af919240f3d03fc61b20242596d19133486
Life1sOk <mr.lifeisok@gmail.com>
2023-06-08 18:15:27 +0600

    main vers

--- src/utils/contollers/useConnectSse.ts
+++ src/utils/contollers/useConnectSse.ts
@@ -12,9 +12,12 @@ export const useConnectSse = () => {
   const { addReceivingLocal } = useBoxLocal();
 
   const getMessage = async () => {
-    const eventSource = new EventSource(`http://localhost:3000/connect/${userId}`, {
-      withCredentials: true,
-    });
+    const eventSource = new EventSource(
+      `https://link-saver.herokuapp.com/connect/${userId}`,
+      {
+        withCredentials: true,
+      }
+    );
 
     eventSource.onmessage = function (event) {
       const message = JSON.parse(event.data);
