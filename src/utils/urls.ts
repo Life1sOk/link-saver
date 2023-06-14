249952329b8e4692101b42229913caf1e079f622
Life1sOk <mr.lifeisok@gmail.com>
2023-06-14 08:49:02 +0600

    check

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,4 +1,5 @@
 // export const currentUrl = "https://link-saver.herokuapp.com";
 export const currentUrl = "http://localhost:3000";
 
-export const socketUrl = "ws://localhost:3000/connect";
+// export const socketUrl = "ws://localhost:3000/connect";
+export const socketUrl = "wss://link-saver.herokuapp.com/connect";
