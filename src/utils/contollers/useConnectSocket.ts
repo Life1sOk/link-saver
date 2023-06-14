3c202cfecdf906c2c77765347cd93aa545e3fefa
Life1sOk <mr.lifeisok@gmail.com>
2023-06-14 10:06:49 +0600

    qwe

--- src/utils/contollers/useConnectSocket.ts
+++ src/utils/contollers/useConnectSocket.ts
@@ -10,48 +10,52 @@ export const useWebsocket = () => {
   const { addOneToListLocal, deleteOneFromListLocal } = useFriendsLocal();
   const { addReceivingLocal } = useBoxLocal();
 
+  const websocketHandler = () => {
+    const socket = new WebSocket("wss://link-saver.herokuapp.com/connect");
+
+    const userId = JSON.stringify({ user_id: session.user_id });
+
+    socket.onopen = () => socket.send(userId);
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+
+      console.log(message);
+
+      switch (message.type) {
+        // Lists: friends, invited, incoming, search
+        case "invite friend":
+          addOneToListLocal({ which: "incoming", user: message.data });
+          break;
+        case "confirmed friend":
+          deleteOneFromListLocal({ from: "invited", id: message.data.id });
+          addOneToListLocal({ which: "friends", user: message.data });
+          break;
+        case "delete friend":
+          deleteOneFromListLocal({ from: "friends", id: message.data.from_id });
+          break;
+        case "transition box":
+          addReceivingLocal(message.data);
+          break;
+      }
+    };
+
+    console.log("connected");
+
+    socket.onclose = (event) => {
+      if (event.wasClean) {
+        alert(
+          `[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`
+        );
+      } else {
+        // например, сервер убил процесс или сеть недоступна
+        // обычно в этом случае event.code 1006
+        console.log("connection was closed", event.code);
+        setTimeout(websocketHandler, 1000);
+      }
+    };
+  };
+
   useEffect(() => {
-    if (session.success) {
-      const socket = new WebSocket("wss://link-saver.herokuapp.com/connect");
-
-      const userId = JSON.stringify({ user_id: session.user_id });
-
-      // socket.onclose = () => console.log("connection closed");
-      socket.onopen = () => socket.send(userId);
-      socket.onmessage = (event) => {
-        const message = JSON.parse(event.data);
-
-        console.log(message);
-
-        switch (message.type) {
-          // Lists: friends, invited, incoming, search
-          case "invite friend":
-            addOneToListLocal({ which: "incoming", user: message.data });
-            break;
-          case "confirmed friend":
-            deleteOneFromListLocal({ from: "invited", id: message.data.id });
-            addOneToListLocal({ which: "friends", user: message.data });
-            break;
-          case "delete friend":
-            deleteOneFromListLocal({ from: "friends", id: message.data.from_id });
-            break;
-          case "transition box":
-            addReceivingLocal(message.data);
-            break;
-        }
-      };
-
-      socket.onclose = (event) => {
-        if (event.wasClean) {
-          alert(
-            `[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`
-          );
-        } else {
-          // например, сервер убил процесс или сеть недоступна
-          // обычно в этом случае event.code 1006
-          alert("[close] Соединение прервано" + event.code);
-        }
-      };
-    }
+    if (session.success) websocketHandler();
   }, [session.success]);
 };
